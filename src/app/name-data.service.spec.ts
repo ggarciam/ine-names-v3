ab32ec21a625c6e76a8254a7e509b3de023a6825
Gonzalo Garcia <gonzalo@sedna.de>
2017-11-22 17:31:13 +0100

    Created api service, names service and modified components, now showing names in a list

---
+++ src/app/name-data.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { NameDataService } from './name-data.service';
+
+describe('NameDataService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [NameDataService]
+    });
+  });
+
+  it('should be created', inject([NameDataService], (service: NameDataService) => {
+    expect(service).toBeTruthy();
+  }));
+});
